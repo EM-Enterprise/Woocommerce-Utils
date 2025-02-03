479bda34948311849360c17f2020a63f278bf8d4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-03 16:22:18 +0100

    feat: created getProducts functionality

---
+++ functions/products/getProducts.ts
@@ -0,0 +1,11 @@
+import { useWoocommerce } from '@/api/useWoocommerce'
+import { Product, validateProducts } from '@/schemas/products/Product'
+import { ApiFilterProps, validateApiFilterProps } from '@/schemas/ApiFilterProps'
+
+export async function getProducts(filterProps: ApiFilterProps): Promise<Product[] | never> {
+  const filters = validateApiFilterProps(filterProps)
+  const { get } = useWoocommerce()
+
+  const products = await get<Product[]>('products', filters)
+  return validateProducts(products)
+}
