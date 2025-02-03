e2f3b0bea1ab6dfb93c1a6bee63874061c4dffc4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-03 17:11:33 +0100

    ref: moved ApiFilterProps to /schemas/filters

--- functions/orders/getOrders.ts
+++ functions/orders/getOrders.ts
@@ -1,6 +1,6 @@
 import { useWoocommerce } from '@/api/useWoocommerce'
 import { Order, validateOrders } from '@/schemas/orders/Order'
-import { ApiFilterProps, validateApiFilterProps } from '@/schemas/ApiFilterProps'
+import { ApiFilterProps, validateApiFilterProps } from '@/schemas/filters/ApiFilterProps'
 
 export async function getOrders(filterProps: ApiFilterProps = {}): Promise<Order[] | never> {
   const filters = validateApiFilterProps(filterProps)
--- functions/products/getProducts.ts
+++ functions/products/getProducts.ts
@@ -1,6 +1,6 @@
 import { useWoocommerce } from '@/api/useWoocommerce'
 import { Product, validateProducts } from '@/schemas/products/Product'
-import { ApiFilterProps, validateApiFilterProps } from '@/schemas/ApiFilterProps'
+import { ApiFilterProps, validateApiFilterProps } from '@/schemas/filters/ApiFilterProps'
 
 export async function getProducts(filterProps: ApiFilterProps): Promise<Product[] | never> {
   const filters = validateApiFilterProps(filterProps)
