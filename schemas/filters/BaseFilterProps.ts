b8d1edb465641e5191668a69e13ecdcecdf4ce29
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-04 17:19:11 +0100

    ref: changed exclude, include type in BaseFilter schema
    The reason for this change is that the exclusion and include arrays focus on the ids of specific elements, which are also of type number. While providing these ids as strings works too, defining them as numbers is more makes more sense, to avoid confusion.

--- schemas/filters/BaseFilterProps.ts
+++ schemas/filters/BaseFilterProps.ts
@@ -4,8 +4,8 @@ import { stripZodDefault } from '@/schemas/utils/stripZodDefaultValues'
 const BaseFilterPropsSchema = z.object({
   page: z.number().min(1, { message: 'The page must be at least 1.' }).default(1).optional(),
   per_page: z.number().min(1).max(1000, { message: 'The per_page value must not be larger than 1000' }).default(10).optional(),
-  exclude: z.array(z.string()).optional(),
-  include: z.array(z.string()).optional(),
+  exclude: z.array(z.number()).optional(),
+  include: z.array(z.number()).optional(),
   offset: z.number().min(1).optional(),
   orderby: z.enum(['date', 'modified', 'id', 'include', 'title', 'slug']).default('date').optional(),
 })
