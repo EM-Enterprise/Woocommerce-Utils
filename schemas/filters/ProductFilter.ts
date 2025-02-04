e5ce6fecb4f4a8a12af574618ef325478167d26d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-04 09:04:37 +0100

    ref: updated ProductFilterProps schema
    Changed some the property types and moved the optional flag to the end.

--- schemas/filters/ProductFilter.ts
+++ schemas/filters/ProductFilter.ts
@@ -1,34 +1,20 @@
 import { extendApiFilterPropsSchema } from '@/schemas/filters/ApiFilterProps'
 import { z } from 'zod'
 
-const ProductFilterPropsSchema = extendApiFilterPropsSchema(
-  z.object({
-    status: z.enum(['draft', 'pending', 'private', 'publish', 'any']).optional().default('any'),
-    include_status: z
-      .array(z.enum(['draft', 'pending', 'private', 'publish', 'any']))
-      .transform((states) => states.join(','))
-      .optional(),
-    exclude_status: z
-      .array(z.enum(['draft', 'pending', 'private', 'publish', 'any']))
-      .transform((states) => states.join(','))
-      .optional(),
+const ProductFilterPropsSchema = extendApiFilterPropsSchema({
+  status: z.enum(['draft', 'pending', 'private', 'publish', 'any']).default('any').optional(),
+  include_status: z.array(z.enum(['draft', 'pending', 'private', 'publish', 'any'])).optional(),
+  exclude_status: z.array(z.enum(['draft', 'pending', 'private', 'publish', 'any'])).optional(),
 
-    type: z.enum(['simple', 'variable', 'grouped', 'external']).optional(),
-    include_types: z
-      .array(z.enum(['simple', 'variable', 'grouped', 'external']))
-      .transform((types) => types.join(','))
-      .optional(),
-    exclude_types: z
-      .array(z.enum(['simple', 'variable', 'grouped', 'external']))
-      .transform((types) => types.join(','))
-      .optional(),
+  type: z.enum(['simple', 'variable', 'grouped', 'external']).optional(),
+  include_types: z.array(z.enum(['simple', 'variable', 'grouped', 'external'])).optional(),
+  exclude_types: z.array(z.enum(['simple', 'variable', 'grouped', 'external'])).optional(),
 
-    sku: z.string().optional(),
-    slug: z.string().optional(),
-    shipping_class: z.string().optional(),
-    stock_status: z.enum(['instock', 'outofstock', 'onbackorder']).optional(),
-  }),
-)
+  sku: z.string().optional(),
+  slug: z.string().optional(),
+  shipping_class: z.string().optional(),
+  stock_status: z.enum(['instock', 'outofstock', 'onbackorder']).optional(),
+})
 
 export type ProductFilterProps = z.infer<typeof ProductFilterPropsSchema>
 
